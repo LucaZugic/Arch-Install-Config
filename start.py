62f9df45755a40fce6bdd9f185c552aa4d836bcb
Luca Zugic <zugic.luca@gmail.com>
2021-01-07 00:20:00 +0000

    Commit scripts

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 from os import system
-startx = input("\nHello Luca!\n\nDo you want to start x session? [Y/n]: ")
-if startx == "" or elif startx == "Y" or startx == "y":
+# You might want to add your name after Hello in the following line.
+startx = input("\nHello!\n\nDo you want to start x session? [Y/n]: ")
+if startx == '' or elif startx == 'Y' or startx == 'y':
     system("startx")
