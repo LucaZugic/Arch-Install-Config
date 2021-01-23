7de287eb2d8ebd7fad3851e72d46c05cb796a60b
Luca Zugic <62926784+LucaZugic@users.noreply.github.com>
2021-01-23 20:54:00 +0000

    Delete start.py

--- start.py
+++
@@ -1,5 +0,0 @@
-from os import system
-# You might want to add your name after Hello in the following line.
-startx = input("\nHello!\n\nDo you want to start x session? [Y/n]: ")
-if startx == '' or elif startx == 'Y' or startx == 'y':
-    system("startx")
